8c2687611100c80092f20c808735414960e3994f
Carmen Lopez <carmen.lopez@gmail.com>
2019-08-24 19:01:49 +0200

    WIP added Beer domain Entity

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizerImpl.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizerImpl.java
@@ -5,22 +5,46 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+@Component
 
 public class BlockingRestTemplateCustomizerImpl implements RestTemplateCustomizer {
 
+    private Integer maxTotalConnection;
+
+    private Integer defaultmaxTotalConnections;
+
+    private Integer connectionRequestTimeout;
+
+    private Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizerImpl(
+            @Value("${sfg.maxtotalconnections}") Integer maxTotalConnection,
+            @Value("${sfg.defaultmaxtotalconnections}") Integer defaultmaxTotalConnections,
+            @Value("${sfg.connectionrequesttimeout}") Integer connectionRequestTimeout,
+            @Value("${sfg.sockettimeout}") Integer socketTimeout
+    ) {
+        this.maxTotalConnection=maxTotalConnection;
+        this.defaultmaxTotalConnections=defaultmaxTotalConnections;
+        this.connectionRequestTimeout=connectionRequestTimeout;
+        this.socketTimeout=socketTimeout;
+    }
+
     public ClientHttpRequestFactory clientHttpRequestFactory() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotalConnection);
+        connectionManager.setDefaultMaxPerRoute(defaultmaxTotalConnections);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
                 .build();
         CloseableHttpClient httpClient = HttpClients
                 .custom()
---
+++ src/main/java/guru/springframework/msscbreweryclient/web/domain/Beer.java
@@ -0,0 +1,48 @@
+package guru.springframework.msscbreweryclient.web.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.security.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lasModifiedDate;
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
